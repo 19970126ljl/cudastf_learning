e643d746c67ef80adef7071d57b5e432f78a139d
19970126ljl <1436116748@qq.com>
2025-09-01 10:45:24 +0000

    add maxregcount support

---
+++ stf_paper_evaluation/multi-gpu-reduction/kernels/stf_launch_maxregcount.cu
@@ -0,0 +1,40 @@
+#include "kernels/stf_launch_maxregcount.cuh"
+
+void benchmark_stf_launch_maxregcount(unsigned long long N, thrust::device_vector<double>& d_X, double ref_sum, bool verify_with_cpu, int num_iterations, std::ofstream& csv_file) {
+    double total_time = 0.0;
+    double result = 0.0;
+    double* d_input_stf = d_X.data().get();
+    CUDA_CHECK(cudaSetDevice(0));
+
+    // Warm-up
+    {
+        context ctx;
+        double sum = 0.0;
+        auto lX = ctx.logical_data(d_input_stf, {N}, data_place::device());
+        auto lsum = ctx.logical_data(&sum, {1});
+        stf_launch_maxregcount(ctx, lX, lsum);
+        cudaStream_t stream = ctx.fence();
+        cudaStreamSynchronize(stream);
+        ctx.finalize();
+    }
+
+    GPUTimer timer;
+    for (int i = 0; i < num_iterations; i++) {
+        context ctx;
+        double sum = 0.0;
+        auto lX = ctx.logical_data(d_input_stf, {N}, data_place::device());
+        auto lsum = ctx.logical_data(&sum, {1});
+
+        timer.start("CUDA STF MAXREG");
+        stf_launch_maxregcount(ctx, lX, lsum);
+        cudaStream_t stream = ctx.fence();
+        cudaStreamSynchronize(stream);
+        timer.stop();
+        ctx.finalize();
+        result = sum;
+        total_time += timer.elapsed_milliseconds();
+    }
+
+    double avg_time = total_time / num_iterations;
+    report_results("CUDA STF MAXREG", N, avg_time, result, ref_sum, verify_with_cpu, csv_file);
+}
